d1f938c9ae5eee1dd10d30c8120487754c5ec934
Mattia de' Michieli Vitturi <demichie@gmail.com>
2024-09-05 09:13:42 +0200

    Update topoGrid.C

--- applications/utilities/topoGrid/topoGrid.C
+++ applications/utilities/topoGrid/topoGrid.C
@@ -63,7 +63,6 @@ int main(int argc, char *argv[])
     const scalar xVent = topoDict.lookupOrDefault<scalar>("xVent",0.0);
     const scalar yVent = topoDict.lookupOrDefault<scalar>("yVent",0.0);
     const scalar expFactor = topoDict.lookupOrDefault<scalar>("expFactor",1.0);
-    const scalar dzTop = topoDict.lookupOrDefault<scalar>("dzTop",0.0);
     const scalar dzVert = topoDict.lookupOrDefault<scalar>("dzVert",0.0);
     const scalar exp_shape = topoDict.lookupOrDefault<scalar>("exp_shape",1.0);
 
@@ -129,7 +128,7 @@ int main(int argc, char *argv[])
             if (value == NODATA_value)
                 value = 0.0;  // Handle NODATA_value appropriately
 
-            elevation(nrows-1-i, j) = value;
+            elevation(i, j) = value;
         }
     }
 
@@ -140,12 +139,8 @@ int main(int argc, char *argv[])
 
     file.close();
 
-    const volPointInterpolation& pInterp(volPointInterpolation::New(mesh));
-
     // Get times list
     instantList Times = runTime.times();
-
-    pointField zeroPoints(mesh.points());
 
     // skip "constant" time
     for (label timeI = 1; timeI < Times.size(); ++timeI)
@@ -154,74 +149,30 @@ int main(int argc, char *argv[])
 
         Info<< "Time = " << runTime.userTimeName() << endl;
 
-    // Create a volScalarField with the dimension of length for storing interpolated elevation data
-    volScalarField Uz
-    (
-        IOobject
-        (
-            "Uz",
-            runTime.name(),
-            mesh,
-            IOobject::NO_READ,
-            IOobject::NO_WRITE
-        ),
-        mesh,
-        dimensionedScalar("zero", dimensionSet(0, 1, 0, 0, 0), 0.0)
-    );
-
-    volScalarField Ux
-    (
-        IOobject
-        (
-            "Ux",
-            runTime.name(),
-            mesh,
-            IOobject::NO_READ,
-            IOobject::NO_WRITE
-        ),
-        mesh,
-        dimensionedScalar("zero", dimensionSet(0, 1, 0, 0, 0), 0.0)
-    );
-    
-    volScalarField Uy
-    (
-        IOobject
-        (
-            "Uy",
-            runTime.name(),
-            mesh,
-            IOobject::NO_READ,
-            IOobject::NO_WRITE
-        ),
-        mesh,
-        dimensionedScalar("zero", dimensionSet(0, 1, 0, 0, 0), 0.0)
-    );    
-
     scalar zMin = min(mesh.Cf().component(2)).value();
     scalar zMax = max(mesh.Cf().component(2)).value();
     
     Info << "zMin = " << zMin << endl;
     Info << "zMax = " << zMax << endl;
     
-    if ( dzTop > 0 )
-    {
-        zMax -= dzTop;
-        Info << "modified zMax = " << zMax << endl;    
-    }
-
     scalar z2Rel(0.0);
     scalar zNew(0.0);
 
-    // Loop over all cells in the mesh to interpolate elevation values
-    forAll(Uz, celli)
-    {
-        // Get the center point of the current cell
-        const point& cellCenter = mesh.C()[celli];
+    pointField zeroPoints(mesh.points());
 
-        // Get x, y coordinates of the cell center
-        scalar x = cellCenter.x();
-        scalar y = cellCenter.y();
-        scalar z = cellCenter.z();
+    pointField pDeform
+    (
+        0.0*zeroPoints
+    );
+
+
+    // Loop over all cells in the mesh to interpolate elevation values
+    forAll(pDeform,pointi)
+    {
+        // Get x, y coordinates of the pointi
+        scalar x = mesh.points()[pointi].x();
+        scalar y = mesh.points()[pointi].y();
+        scalar z = mesh.points()[pointi].z();
         
         scalar zRel = min(1.0, (zMax-z)/(zMax-zMin));
 
@@ -248,7 +199,7 @@ int main(int argc, char *argv[])
                 v11 * xLerp * yLerp;
 
             // Assign interpolated value to the volScalarField U
-            Uz[celli] = zRel * zInterp;
+            pDeform[pointi].z() = zRel * zInterp;
                 
             zNew = z + zRel * zInterp;
                 
@@ -259,148 +210,37 @@ int main(int argc, char *argv[])
                     // enlarge from a fixed height above the maximum
                     // topography and the top, thus from an horizontal
                     // plane to the top
-                    z2Rel = max(0, (zNew - zVert) / (zMax + dzTop - zVert));
+                    z2Rel = max(0, (zNew - zVert) / (zMax - zVert));
                 }
                 else
                 {
                     // enlarge from the topography to the top
-                    z2Rel = (zNew - zInterp) / (zMax + dzTop - zInterp);
+                    z2Rel = (zNew - zInterp) / (zMax - zInterp);
                 }
                 z2Rel = std::pow(z2Rel,exp_shape);
                 
-                Ux[celli] = z2Rel*(expFactor-1.0)*x;
-                Uy[celli] = z2Rel*(expFactor-1.0)*y;
+                pDeform[pointi].x() = z2Rel*(expFactor-1.0)*x;
+                pDeform[pointi].y() = z2Rel*(expFactor-1.0)*y;
             }
             else
             {
-                Ux[celli] = 0.0;
-                Uy[celli] = 0.0;
+                pDeform[pointi].x() = 0.0;
+                pDeform[pointi].y() = 0.0;
             }        
         }
         else
         {
             // If outside the raster bounds, set to a default value (e.g., 0)
-            Uz[celli] = 0.0;
-            Ux[celli] = 0.0;
-            Uy[celli] = 0.0;
+            pDeform[pointi].z() = 0.0;
+            pDeform[pointi].x() = 0.0;
+            pDeform[pointi].y() = 0.0;
         }
     }
-
-
-    // Loop over all boundary faces to interpolate elevation values
-    forAll(mesh.boundary(), patchi)
-    {
-        const fvPatch& boundaryPatch = mesh.boundary()[patchi];
-        const labelList& faceCells = boundaryPatch.faceCells();
-
-        forAll(faceCells, facei)
-        {
-            // Get the center point of the current boundary face
-            const point& faceCenter = boundaryPatch.Cf()[facei];
-
-            // Get x, y coordinates of the face center
-            scalar x = faceCenter.x();
-            scalar y = faceCenter.y();
-            scalar z = faceCenter.z();
-
-            scalar zRel = max(0.0,min(1.0, (zMax-z)/(zMax-zMin)));
-
-            // Calculate row and column indices in the elevation matrix
-            int colIndex = (x - xllcorner) / cellsize;
-            int rowIndex = (y - yllcorner) / cellsize;
-
-            // Interpolate elevation value
-            if (colIndex >= 0 && colIndex <= ncols  && rowIndex >= 0 && rowIndex <= nrows )
-            {
-                // Bilinear interpolation
-                scalar xLerp = (x - (xllcorner + colIndex * cellsize)) / cellsize;
-                scalar yLerp = (y - (yllcorner + rowIndex * cellsize)) / cellsize;
-
-                scalar v00 = elevation(rowIndex, colIndex);
-                scalar v01 = elevation(rowIndex, colIndex + 1);
-                scalar v10 = elevation(rowIndex + 1, colIndex);
-                scalar v11 = elevation(rowIndex + 1, colIndex + 1);
-
-                scalar zInterp = 
-                    v00 * (1 - xLerp) * (1 - yLerp) +
-                    v01 * xLerp * (1 - yLerp) +
-                    v10 * (1 - xLerp) * yLerp +
-                    v11 * xLerp * yLerp;
-
-                // Assign interpolated value to the volScalarField U
-                Uz.boundaryFieldRef()[patchi][facei] = zRel * zInterp;
-                
-                zNew = z + zRel * zInterp;
-                
-                if ( z>= 0.0)
-                {
-                    if (dzVert > 0)
-                    {
-                        // enlarge from a fixed height above the maximum
-                        // topography and the top, thus from an horizontal
-                        // plane to the top
-                        z2Rel = max(0, (zNew - zVert) / (zMax + dzTop - zVert));
-                    }
-                    else
-                    {
-                        // enlarge from the topography to the top
-                        z2Rel = (zNew - zInterp) / (zMax + dzTop - zInterp);
-                    }
-                    z2Rel = std::pow(z2Rel,exp_shape);
-                
-                    Ux.boundaryFieldRef()[patchi][facei] = z2Rel*(expFactor-1.0)*x;
-                    Uy.boundaryFieldRef()[patchi][facei] = z2Rel*(expFactor-1.0)*y;
-                }
-                else
-                {
-                    Ux.boundaryFieldRef()[patchi][facei] = 0.0;
-                    Uy.boundaryFieldRef()[patchi][facei] = 0.0;
-                }        
-            }
-            else
-            {
-                // If outside the raster bounds, set to a default value (e.g., 0)
-                Uz.boundaryFieldRef()[patchi][facei] = 0.0;
-                Ux.boundaryFieldRef()[patchi][facei] = 0.0;
-                Uy.boundaryFieldRef()[patchi][facei] = 0.0;
-            }
-        }
-    }
-    
-    // Now create a volVectorField V, with (X,Y,Z) = (0,0,U)
-    volVectorField Udeform
-    (
-        IOobject
-        (
-            "Udeform",
-            runTime.name(),
-            mesh,
-            IOobject::NO_READ,
-            IOobject::AUTO_WRITE
-        ),
-        mesh,
-        dimensionedVector("zero", dimensionSet(0, 1, 0, 0, 0), vector::zero)
-    );
-    
-    // Loop over all cells in the mesh to set (X,Y,Z) = (0,0,Uz)
-    forAll(Udeform, celli)
-    {
-        Udeform[celli] = vector(Ux[celli], Uy[celli], Uz[celli]);
-    }
-
-    // Loop over all boundary faces to set (X,Y,Z) = (0,0,Uz)
-    forAll(mesh.boundary(), patchi)
-    {
-        forAll(mesh.boundary()[patchi], facei)
-        {
-            Udeform.boundaryFieldRef()[patchi][facei] = vector(Ux.boundaryField()[patchi][facei],
-                      Uy.boundaryField()[patchi][facei], Uz.boundaryField()[patchi][facei]);
-        }
-    }        
+           
     
     pointField newPoints
     (
-        zeroPoints + pInterp.interpolate(Udeform)().primitiveField()
+        zeroPoints + pDeform
     );
 
     mesh.setPoints(newPoints);
